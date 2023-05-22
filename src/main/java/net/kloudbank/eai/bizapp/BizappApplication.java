e076ce1ce9e9fa6b54dfcbf3027a360099ce645c
nogada <seungil.han@gmail.com>
2023-05-22 10:36:21 +0900

    init

---
+++ src/main/java/net/kloudbank/eai/bizapp/BizappApplication.java
@@ -0,0 +1,13 @@
+package net.kloudbank.eai.bizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/kloudbank/eai/bizapp/BizappApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kloudbank.eai.bizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
